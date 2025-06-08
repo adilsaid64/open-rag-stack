365aff2b8ad1eec595cf93822f7b6ad78b5cb8a2
adil <adilsaid64@gmail.com>
2025-06-08 16:37:57 +0100

    feat: lakefs client

--- rag-orchestrator/app/src/lakefs_store.py
+++ rag-orchestrator/app/src/lakefs_store.py
@@ -1,5 +1,29 @@
-# lakefs_logger.py
+from lakefs.client import Client
 
 
 class LakeFSLogger:
     """Commits documents to lakefs"""
+
+    def __init__(
+        self,
+        lakefs_endpoint: str,
+        lakefs_username: str,
+        lakefs_password: str,
+        lakefs_repo: str,
+    ):
+        self.lakefs_endpoint = lakefs_endpoint
+        self.lakefs_username = lakefs_username
+        self.lakefs_repo = lakefs_repo
+
+        self.lakefs_client = Client(
+            host=self.lakefs_endpoint,
+            username=lakefs_username,
+            password=lakefs_password,
+        )
+        self.lakfs_repo = ...
+
+        self.lakefs_branch = ...
+
+    def upload_document(self, document: str):
+        """Uploads a document to lakefs and returns a commit hash"""
+        ...
